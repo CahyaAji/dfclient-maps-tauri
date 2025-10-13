0e3eac83345734c8f4dea58b550f66191309ec42
CahyaAji <cahya.akk@gmail.com>
2025-10-13 11:04:05 +0700

    add frequency set

--- src/lib/components/ControlPanel.svelte
+++ src/lib/components/ControlPanel.svelte
@@ -1,9 +1,17 @@
-<div class="container">Control Panel</div>
+<script>
+    import FreqGainSetup from "./FreqGainSetup.svelte";
+</script>
+
+<div class="container">
+    <FreqGainSetup />
+</div>
 
 <style>
     .container {
         padding: 2px 8px 16px 8px;
         color: white;
+        display: flex;
+        flex-direction: column;
         width: 400px;
         height: 200px;
         background-color: rgba(75, 83, 32, 0.6);
---
+++ src/lib/components/FreqGainSetup.svelte
@@ -0,0 +1,10 @@
+<div class="container">isi</div>
+
+<style>
+    .container {
+        padding: 2px 8px 16px 8px;
+        background-color: orange;
+        display: flex;
+        flex-direction: column;
+    }
+</style>
