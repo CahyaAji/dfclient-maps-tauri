f497e16ae1dc3e938748f44b364a0cc538e86197
CahyaAji <cahya.akk@gmail.com>
2025-10-13 11:04:18 +0700

    change style

--- src/lib/components/StatusDF.svelte
+++ src/lib/components/StatusDF.svelte
@@ -2,6 +2,10 @@
 
 <style>
     .container {
+        padding: 2px 8px 16px 8px;
+        color: white;
+        display: flex;
+        flex-direction: column;
         width: 400px;
         height: 200px;
         background-color: rgba(75, 83, 32, 0.6);
