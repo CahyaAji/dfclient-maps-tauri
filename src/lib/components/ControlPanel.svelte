aaca2c3a6460a58e79bcede625007f739d825baa
CahyaAji <cahya.akk@gmail.com>
2025-10-13 16:05:20 +0700

    edit style

--- src/lib/components/ControlPanel.svelte
+++ src/lib/components/ControlPanel.svelte
@@ -4,16 +4,14 @@
 
 <div class="container">
     <FreqGainSetup />
+    <div>Nav</div>
 </div>
 
 <style>
     .container {
-        padding: 2px 8px 16px 8px;
-        color: white;
         display: flex;
         flex-direction: column;
         width: 400px;
-        height: 200px;
         background-color: rgba(75, 83, 32, 0.6);
         border-radius: 10px;
     }
