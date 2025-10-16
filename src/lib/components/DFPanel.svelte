233cbd094547b0f0bb61f22fea0c8094ee2eb0b2
CahyaAji <cahya.akk@gmail.com>
2025-10-16 15:51:52 +0700

    add init on app start

--- src/lib/components/DFPanel.svelte
+++ src/lib/components/DFPanel.svelte
@@ -2,6 +2,29 @@
     import ControlPanel from "./ControlPanel.svelte";
     import DFPlot from "./DFPlot.svelte";
     import StatusDF from "./StatusDF.svelte";
+
+    let appInitialized = false;
+
+    $effect(() => {
+        console.log("DFPanel, appInitialized: ", appInitialized);
+
+        if (appInitialized) {
+            console.log("DFPanel already initialized, skipping");
+            return () => {
+                console.log("Skipped initialization cleanup - doing nothing");
+            };
+        }
+
+        async function initialize() {
+            appInitialized = true;
+
+            //1. Load ConfigStore
+
+            //2. Start DFStore
+            //3. Start CompassStore
+            //4 Load DF Setting
+        }
+    });
 </script>
 
 <div class="container">
