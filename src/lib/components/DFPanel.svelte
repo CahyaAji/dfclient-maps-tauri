5b2b21cf50ba2c3c49b6c045ba6cdceafdd56684
CahyaAji <cahya.akk@gmail.com>
2025-10-21 01:24:34 +0700

    add initialize on start

--- src/lib/components/DFPanel.svelte
+++ src/lib/components/DFPanel.svelte
@@ -1,49 +1,66 @@
 <script>
-    import ControlPanel from "./ControlPanel.svelte";
-    import DFPlot from "./DFPlot.svelte";
-    import StatusDF from "./StatusDF.svelte";
+  import { dfStore } from "../stores/dfStore.svelte";
+  import ControlPanel from "./ControlPanel.svelte";
+  import DFPlot from "./DFPlot.svelte";
+  import StatusDF from "./StatusDF.svelte";
 
-    let appInitialized = false;
+  let appInitialized = false;
 
-    $effect(() => {
-        console.log("DFPanel, appInitialized: ", appInitialized);
+  $effect(() => {
+    console.log("DFPanel, appInitialized: ", appInitialized);
 
-        if (appInitialized) {
-            console.log("DFPanel already initialized, skipping");
-            return () => {
-                console.log("Skipped initialization cleanup - doing nothing");
-            };
-        }
+    if (appInitialized) {
+      console.log("DFPanel already initialized, skipping");
+      return () => {
+        console.log("Skipped initialization cleanup - doing nothing");
+      };
+    }
 
-        async function initialize() {
-            appInitialized = true;
+    async function initialize() {
+      appInitialized = true;
 
-            //1. Load ConfigStore
+      //1. Load ConfigStore
+      //2. Start DFStore
+      if (!dfStore.isRunning) {
+        console.log("Starting dfStore");
+        dfStore.start();
+        console.log("dfStore started");
+      } else {
+        console.log("dfStore already running");
+      }
 
-            //2. Start DFStore
-            //3. Start CompassStore
-            //4 Load DF Setting
-        }
-    });
+      //3. Start CompassStore
+      //4 Load DF Setting
+    }
+
+    initialize();
+    return async () => {
+      //stop frequency debounce
+      //stop dfStore
+      dfStore.stop();
+
+      //stop udp listening
+    };
+  });
 </script>
 
 <div class="container">
-    <StatusDF />
-    <DFPlot />
-    <ControlPanel />
+  <StatusDF />
+  <DFPlot />
+  <ControlPanel />
 </div>
 
 <style>
-    .container {
-        position: absolute;
-        display: flex;
-        flex-direction: column;
-        gap: 8px;
-        top: 8px;
-        right: 8px;
-        width: 360px;
-        background-color: transparent;
-        border: 1px solid #ccc;
-        border-radius: 4px;
-    }
+  .container {
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+    top: 8px;
+    right: 8px;
+    width: 320px;
+    background-color: transparent;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+  }
 </style>
