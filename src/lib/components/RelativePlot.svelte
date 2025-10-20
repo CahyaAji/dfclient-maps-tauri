774eb90088ee27083a5f1bf013c9383adb1e7e28
CahyaAji <cahya.akk@gmail.com>
2025-10-21 01:25:48 +0700

    modify to show df data

--- src/lib/components/RelativePlot.svelte
+++ src/lib/components/RelativePlot.svelte
@@ -1,48 +1,64 @@
+<script>
+  import { dfStore } from "../stores/dfStore.svelte";
+</script>
+
 <div class="container">
-    <div class="rotating-circle" style="transform: rotate(45deg);">
-        <div class="arrow"></div>
+  {#if dfStore.data && dfStore.data.heading !== undefined && dfStore.data.heading !== null}
+    <div
+      class="rotating-circle"
+      style="transform: rotate({dfStore.data.heading}deg);"
+    >
+      <div class="arrow"></div>
     </div>
-    <div class="angle-text"><div>- - -</div></div>
+  {/if}
+  <div class="angle-text">
+    {#if dfStore.data && dfStore.data.heading !== undefined && dfStore.data.heading !== null}
+      <div>{dfStore.data.heading}</div>
+    {:else}
+      <div>---</div>
+    {/if}
+  </div>
 </div>
 
 <style>
-    .container {
-        display: flex;
-        width: 250px;
-        height: 250px;
-        background-image: url("/src/assets/relative_circle.png");
-        background-repeat: no-repeat;
-        background-size: cover;
-        background-color: rgba(4, 61, 15, 0.5);
-        border-radius: 50%;
-        position: relative;
-        align-items: center;
-        justify-content: center;
-    }
-    .rotating-circle {
-        background-color: transparent;
-        width: 190px;
-        height: 190px;
-        border-radius: 50%;
-    }
-    .arrow {
-        width: 8px;
-        height: 63px;
-        background-color: yellow;
-        margin: auto;
-        border-radius: 4px;
-    }
-    .angle-text {
-        width: 70px;
-        height: 70px;
-        display: flex;
-        position: absolute;
-        align-items: center;
-        justify-content: center;
-        border-radius: 50%;
-        border: 2px solid white;
-        color: white;
-        background-color: rgba(4, 61, 15, 0.5);
-        color: yellow;
-    }
+  .container {
+    display: flex;
+    width: 230px;
+    height: 230px;
+    background-image: url("/src/assets/relative_circle.png");
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-color: rgba(4, 61, 15, 0.5);
+    border-radius: 50%;
+    position: relative;
+    align-items: center;
+    justify-content: center;
+  }
+  .rotating-circle {
+    background-color: transparent;
+    width: 180px;
+    height: 180px;
+    border-radius: 50%;
+  }
+  .arrow {
+    width: 8px;
+    height: 58px;
+    background-color: yellow;
+    margin: auto;
+    border-radius: 4px;
+  }
+  .angle-text {
+    width: 70px;
+    height: 70px;
+    display: flex;
+    position: absolute;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+    border: 2px solid white;
+    color: white;
+    background-color: rgba(4, 61, 15, 0.5);
+    color: yellow;
+    font-size: 18pt;
+  }
 </style>
