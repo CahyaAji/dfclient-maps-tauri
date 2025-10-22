cd512d1daf493b052b83cd6a02d89bd60898efdb
CahyaAji <cahya.akk@gmail.com>
2025-10-22 15:47:52 +0700

    add for dev

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,17 @@
 <script>
   import DFPanel from "./lib/components/DFPanel.svelte";
   import Maps from "./lib/components/Maps.svelte";
+
+  import { isTauriRuntime } from "./lib/utils/devhelper";
+
+  isTauriRuntime().then(async (tauri) => {
+    console.log("Running in:", tauri ? "Tauri" : "Browser");
+
+    if (tauri) {
+      const { fetch } = await import("@tauri-apps/plugin-http");
+      console.log("Plugin fetch loaded:", typeof fetch);
+    }
+  });
 </script>
 
 <main>
