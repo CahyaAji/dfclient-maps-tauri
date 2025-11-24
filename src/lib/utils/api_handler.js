e29bd2d4c86253384ac1b59006a10488d8be268b
CahyaAji <cahya.akk@gmail.com>
2025-11-24 09:04:25 +0700

    change for build

--- src/lib/utils/api_handler.js
+++ src/lib/utils/api_handler.js
@@ -1,8 +1,8 @@
 // import { fetch } from "@tauri-apps/plugin-http";
 import { unifiedFetch as fetch } from "./devhelper";
 
-// export const API_URL = "http://192.168.17.17:8087";
-export const API_URL = "http://localhost:3000";
+export const API_URL = "http://192.168.17.17:8087";
+// export const API_URL = "http://localhost:3000";
 
 export const readDF = async () => {
   try {
